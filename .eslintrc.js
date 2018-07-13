b9d9871c0d0ba2bb22d6de33009978258cdc248f
Lucas Bicudo <lucbic@gmail.com>
2018-07-13 19:30:02 -0300

    Add ESlint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "es6": true,
+        "amd": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "strict": 0,
+        "no-console":0,
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
