0d62110db01c49b391f93894742514e0f30ce65f
Lucas Bicudo <lucbic@gmail.com>
2018-07-14 17:57:52 -0300

    Add createSlides function

--- src/app.js
+++ src/app.js
@@ -47,6 +47,8 @@ function formatConditions (str) {
 }
 
 function populate (data) {
+  createSlides(data.recommendation)
+
   const itemImg = document.querySelector('.item__img')
   const itemDescription = document.querySelector('.item__description')
   const itemPrice = document.querySelector('.item__price-tag')
@@ -56,4 +58,56 @@ function populate (data) {
   itemDescription.textContent = showLess(90, data.item.name)
   itemPrice.textContent = data.item.price
   itemConditions.textContent = formatConditions(data.item.productInfo.paymentConditions)
+}
+
+function createSlides (recommendation) {
+  let swiperWrapper = document.querySelector('.swiper-wrapper')
+
+  recommendation.forEach( (item, index) => {
+    // create new slide
+    let newSlide = document.createElement('div')
+    newSlide.className = 'swiper-slide recommendation'
+
+    // create new slides attributes
+    let img = document.createElement('img')
+    img.className = 'rec__img'
+
+    let description = document.createElement('p')
+    description.className = 'rec__description'
+
+    let price = document.createElement('p')
+    price.className = 'rec__price'
+
+    let priceTag = document.createElement('span')
+    priceTag.className = 'rec__price-tag'
+    price.appendChild(document.createTextNode('Por: '))
+    price.appendChild(priceTag)
+
+    let conditions = document.createElement('p')
+    conditions.className = 'rec__conditions'
+
+    let conditionsTag = document.createElement('span')
+    conditionsTag.className = 'rec__conditions-tag'
+    conditions.appendChild(document.createTextNode('ou '))
+    conditions.appendChild(conditionsTag)
+    conditions.appendChild(document.createTextNode(' sem juros'))
+
+    // add json data to attributes
+    img.src = imgPath(recommendation[index].imageName)
+    description.textContent = showLess(90, recommendation[index].name)
+    priceTag.textContent = recommendation[index].price
+    conditionsTag.textContent =
+      formatConditions(recommendation[index].productInfo.paymentConditions)
+
+    // append attributes to new slide
+    newSlide.appendChild(img)
+    newSlide.appendChild(description)
+    newSlide.appendChild(price)
+    newSlide.appendChild(conditions)
+
+    // append new slide to swiper
+    swiperWrapper.appendChild(newSlide)
+  })
+
+  mySwiper.update()
 }
